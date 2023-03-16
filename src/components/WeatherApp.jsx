cc40c87be3f36704a9a7005e150be72a3f6864d9
Oliver_rodriguez29 <mawiis455@gmail.com>
2023-03-16 13:19:31 -0500

    listo

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 
-const WeatherApp = ({ weather,temp }) => {
-  const [farehails, setFarehails] = useState(true)
-  const handelClickTemp = () =>{
-    setFarehails(!farehails)
-  }
+const WeatherApp = ({ weather, temp }) => {
+  const [farehails, setFarehails] = useState(true);
+  const handelClickTemp = () => {
+    setFarehails(!farehails);
+  };
 
+  console.log(weather);
   return (
     <div className="weather__contains">
       <h1 className="title__contain">WeatherApp</h1>
@@ -17,20 +18,29 @@ const WeatherApp = ({ weather,temp }) => {
           src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`}
           alt=""
         />
-        <p> {farehails ? temp?.celcius + ' ºC' : temp?.farenheil + ' ºF' }
-
-        </p>
+        <p className="temp"> {farehails ? temp?.celcius + " ºC" : temp?.farenheil + " ºF"}</p>
       </div>
       <article className="infoApp__contain">
         <h3>{weather?.weather[0].description}</h3>
         <div className="data__contain">
-        <p className="data__text">Wind/Speed: {weather?.wind.speed}m/s</p>
-        <p className="data__text">Clouds: {weather?.clouds.all}%</p>
-        <p className="data__text">Presusure: {weather?.main.pressure} mb</p>
+          <div>
+            <i className="bx bx-wind"></i>
+            <p className="data__text">Wind/Speed: {weather?.wind.speed}m/s</p>
+          </div>
+          <div>
+            <i className="bx bx-cloud-drizzle"></i>
+            <p className="data__text">Clouds: {weather?.clouds.all}%</p>
+          </div>
+          <div>
+            <i className="bx bxl-graphql"></i>
+            <p className="data__text">Pressure: {weather?.main.pressure} mb</p>
+          </div>
         </div>
       </article>
       <>
-      <button className="button__app" onClick={handelClickTemp}>change to {!farehails ? 'ºC' : 'ºF'}</button>
+        <button className="button__app" onClick={handelClickTemp}>
+          change to {!farehails ? "ºC" : "ºF"}
+        </button>
       </>
     </div>
   );
